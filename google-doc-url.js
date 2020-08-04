802a1cffd4037d9ed2a7477a0dd4d86826f2685e
Jiří Baňka <69214596+jiribanka@users.noreply.github.com>
2020-08-04 20:33:55 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ns03GjTY5wGT2YnlTRDrt48Su-5m7KN95GJNFfr9Uh0/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ns03GjTY5wGT2YnlTRDrt48Su-5m7KN95GJNFfr9Uh0/edit#gid=0';
